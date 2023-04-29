a6d4df2b4ce1aadf912b32c5c37b959ad749ceb3
Ciro Maccarone <96290740+Pingu2123@users.noreply.github.com>
2023-04-29 15:34:31 +0200

    Add files via upload

--- Progetto/dama main/connection/connection.ts
+++ Progetto/dama main/connection/connection.ts
@@ -1,15 +1,16 @@
 require ('dotenv').config();
-import { Sequelize } from 'sequelize';
+import { Sequelize } from 'sequelize'
 
-
+//Specifiche del nome del DB lasciato nella variabile di ambiente .env (ancora da creare)
+//Pattern singleton per la creazione di singola istanza protetta di database
 
 export class Database{
     private static instance: Database;
     private connection: Sequelize;
     private constructor() {
         this.connection=new Sequelize(
-            process.env.MYSQL_DATABASE,
-            process.env.MYSQL_USER,
+            process.env.MYSQL_DATABASE!, 
+            process.env.MYSQL_USER!,
             process.env.MYSQL_PASSWORD,
             {
                 host: process.env.MYSQL_HOST,
@@ -18,12 +19,12 @@ export class Database{
             }
         );
     }
-}
 
-public static connection(): Sequelize {
+
+    public static connection(): Sequelize {
     if(!Database.instance){
-        this.instance=new Database;
+        this.instance=new Database();
     }
     return Database.instance.connection;
 }
-
+}
